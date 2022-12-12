ce3f3ba24b07ba4333485e39fe0a63e79d2775ae
chris <58946614+cd-hash@users.noreply.github.com>
2022-12-12 20:09:27 +0000

    updated code formatting in db models to use verticality

--- database/userModel.js
+++ database/userModel.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 // Users Schema
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true},
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
 });
 
 export const user = mongoose.model("user", userSchema)
